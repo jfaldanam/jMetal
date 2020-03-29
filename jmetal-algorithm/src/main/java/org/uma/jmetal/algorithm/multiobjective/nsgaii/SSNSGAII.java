342ddefb800447c9908a0e238894e018420b4e4d
ajnebro <ajnebro@users.noreply.github.com>
2020-03-29 15:48:43 +0200

    Add missing file

---
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/nsgaii/SSNSGAII.java
@@ -0,0 +1,118 @@
+package org.uma.jmetal.algorithm.multiobjective.nsgaii;
+
+import org.uma.jmetal.algorithm.ComponentBasedEvolutionaryAlgorithm;
+import org.uma.jmetal.component.densityestimator.DensityEstimator;
+import org.uma.jmetal.component.densityestimator.impl.CrowdingDistanceDensityEstimator;
+import org.uma.jmetal.component.evaluation.Evaluation;
+import org.uma.jmetal.component.evaluation.impl.SequentialEvaluation;
+import org.uma.jmetal.component.initialsolutioncreation.InitialSolutionsCreation;
+import org.uma.jmetal.component.initialsolutioncreation.impl.RandomSolutionsCreation;
+import org.uma.jmetal.component.ranking.Ranking;
+import org.uma.jmetal.component.ranking.impl.FastNonDominatedSortRanking;
+import org.uma.jmetal.component.replacement.Replacement;
+import org.uma.jmetal.component.replacement.impl.RankingAndDensityEstimatorReplacement;
+import org.uma.jmetal.component.replacement.impl.SteadyStateRankingAndDensityEstimatorReplacement;
+import org.uma.jmetal.component.selection.MatingPoolSelection;
+import org.uma.jmetal.component.selection.impl.NaryTournamentMatingPoolSelection;
+import org.uma.jmetal.component.termination.Termination;
+import org.uma.jmetal.component.variation.impl.CrossoverAndMutationVariation;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.comparator.MultiComparator;
+import org.uma.jmetal.util.observable.impl.DefaultObservable;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/** @author Antonio J. Nebro <antonio@lcc.uma.es> */
+public class SSNSGAII<S extends Solution<?>> extends ComponentBasedEvolutionaryAlgorithm<S> {
+
+  /**
+   * Constructor
+   *
+   * @param problem
+   * @param evaluation
+   * @param initialPopulationCreation
+   * @param termination
+   * @param selection
+   * @param variation
+   * @param replacement
+   */
+  public SSNSGAII(
+          Problem<S> problem,
+          Evaluation<S> evaluation,
+          InitialSolutionsCreation<S> initialPopulationCreation,
+          Termination termination,
+          MatingPoolSelection<S> selection,
+          CrossoverAndMutationVariation<S> variation,
+          SteadyStateRankingAndDensityEstimatorReplacement<S> replacement) {
+    super(
+        "NSGA-II",
+        problem,
+        evaluation,
+        initialPopulationCreation,
+        termination,
+        selection,
+        variation,
+        replacement);
+  }
+
+  /** Constructor */
+  public SSNSGAII(
+      Problem<S> problem,
+      int populationSize,
+      int offspringPopulationSize,
+      CrossoverOperator<S> crossoverOperator,
+      MutationOperator<S> mutationOperator,
+      Termination termination,
+      Ranking<S> ranking) {
+    this.name = "NSGA-II";
+    this.problem = problem;
+    this.observable = new DefaultObservable<>(name);
+    this.attributes = new HashMap<>();
+
+    DensityEstimator<S> densityEstimator = new CrowdingDistanceDensityEstimator<>();
+
+    this.createInitialPopulation = new RandomSolutionsCreation<>(problem, populationSize);
+
+    this.replacement =
+        new RankingAndDensityEstimatorReplacement<>(
+            ranking, densityEstimator, Replacement.RemovalPolicy.oneShot);
+
+    this.variation =
+        new CrossoverAndMutationVariation<>(
+            offspringPopulationSize, crossoverOperator, mutationOperator);
+
+    this.selection =
+        new NaryTournamentMatingPoolSelection<>(
+            2,
+            variation.getMatingPoolSize(),
+            new MultiComparator<>(
+                Arrays.asList(
+                    ranking.getSolutionComparator(), densityEstimator.getSolutionComparator())));
+
+    this.termination = termination;
+
+    this.evaluation = new SequentialEvaluation<>();
+  }
+
+  /** Constructor */
+  public SSNSGAII(
+      Problem<S> problem,
+      int populationSize,
+      int offspringPopulationSize,
+      CrossoverOperator<S> crossoverOperator,
+      MutationOperator<S> mutationOperator,
+      Termination termination) {
+    this(
+        problem,
+        populationSize,
+        offspringPopulationSize,
+        crossoverOperator,
+        mutationOperator,
+        termination,
+        new FastNonDominatedSortRanking<>());
+  }
+}
